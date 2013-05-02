5e4bf56e124ae0cb29bbb3d986d4d7db2e0715aa
Frank Schmidt <fschmidt2011@googlemail.com>
2013-05-02 22:27:59 +0200

    intitializing svs exercise1

---
+++ svs_uebung_01/com/fschmidt/classiccypher/ClassicCypher.py
@@ -0,0 +1,97 @@
+'''
+Created on 10.04.2013
+
+@author: Frank Schmidt
+'''
+import re
+class Main(object):
+    def __init__(self):
+        crypto = '''
+Does increased security provide comfort to paranoid people? Or does security provide some very basic protections 
+that we are naive to believe that we don't need? During this time when the Internet provides essential communication between 
+tens of millions of people and is being increasingly used as a tool for commerce, security becomes a tremendously important 
+issue to deal with. There are many aspects to security and many applications, ranging from secure commerce and payments to 
+private communications and protecting passwords. One essential aspect for secure communications is that of cryptography, 
+which is the focus of this chapter. But it is important to note that while cryptography is necessary for secure communications, 
+it is not by itself sufficient. The reader is advised, then, that the topics covered in this chapter only describe the first 
+of many steps necessary for better security in any number of situations. 
+This paper has two major purposes. The first is to define some of the terms and concepts behind basic cryptographic methods, 
+and to offer a way to compare the myriad cryptographic schemes in use today. The second is to provide some real examples of 
+cryptography in use today. I would like to say at the outset that this paper is very focused on terms, concepts, and schemes 
+in current use and is not a treatise of the whole field. No mention is made here about pre-computerized crypto schemes, the 
+difference between a substitution and transposition cipher, cryptanalysis, or other history. Interested readers should check 
+out some of the books in the references section below for detailed and interesting! background information.'''
+
+        print(crypto.lower())
+        key, encoded = encode(crypto.lower())
+        print(encoded)
+        print("Real key:")
+        keylist = sorted(key.keys())
+        for k in keylist:
+            print("(%s: %s), " % (k, key[k]), end="")
+        print('')
+        print("Calculated key: ")
+        decodedKey = decode(encoded)
+        keylist = sorted(decodedKey.keys(), key=decodedKey.get)
+        for k in keylist:
+            print("(%s: %s), " % (decodedKey[k], k), end="")
+        print('')
+        
+        decText = list(encoded)
+        for i in range(len(decText)):
+            if decText[i] in decodedKey:
+                decText[i] = decodedKey[decText[i]]
+                
+        decodedText = ''.join(decText)
+        print(decodedText)
+        
+        for s in commonTripples:
+            pattern = " (" + s[0:2] + ")" + "([a-z]) | (" + s[0] + "([a-z])"+ s[2] + ") | (" + "([a-z])"+ s[1:3] + ") "
+            print(pattern)
+            m = re.search(pattern, decodedText)
+            if m:
+                print(m.group(0))
+            
+
+from collections import Counter
+import random
+letterFrequency =   {'a':8.167, 'b':1.492, 'c':2.782, 'd':4.253, 'e':12.702, 'f':2.228, 'g':2.015, 'h':6.094, 'i':6.966, 'j':0.153,
+                     'k':0.772, 'l':4.025, 'm':2.406, 'n':6.749, 'o':7.507, 'p':1.929, 'q':0.095, 'r':5.987, 's':6.327, 't':9.056,
+                     'u':2.758, 'v':0.978, 'w':2.360, 'x':0.150, 'y':1.974, 'z':0.074, ' ': 18.000 }
+
+commonPairs =       ['th', 'ea', 'of', 'to', 'in', 'it', 'is', 'be', 'as', 'at', 'so', 'we', 'he', 'by', 'or', 'on', 'do', 'if', 'me', 'my', 'up']
+commonTripples =    ['the', 'est', 'for', 'and', 'his', 'ent', 'tha']
+commonRepetitions = ['ss', 'ee', 'tt', 'ff', 'll', 'mm', 'oo']
+
+def decode(text):
+    charCount = Counter(text)
+    print("Letter Freq in encoded text: ")
+    print(charCount)
+    # we assume that there are only lower case letters encoded in the sequence   
+    for x in [x for x in charCount.keys() if not x in letterFrequency]:
+        charCount.pop(x)
+    sortedFreq      = sorted(letterFrequency, key=letterFrequency.get, reverse=True)
+    sortedCharCount = sorted(charCount, key=charCount.get, reverse=True)
+    key = dict(zip(sortedCharCount, sortedFreq))
+            
+    return key
+
+def encode(text):
+    keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    values = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    
+    # generate a random key
+    random.shuffle(values)
+    key = dict(zip(keys, values));
+
+    t = list(text)
+
+    for i in range(len(t)):
+        if(t[i] in key):
+            t[i] = key[t[i]];
+            
+    return (key, ''.join(t));
+
+if __name__ == '__main__':
+    instance = Main() 
+
