8369d6355bb24571c81454fab2ae20a16d24fcc4
mfroede <marcus.froede@gmx.de>
2013-05-14 21:11:40 +0200

    moors law

--- svs_uebung_01/com/svs/keylength/Aufgabe1_2.py
+++ svs_uebung_01/com/svs/keylength/Aufgabe1_2.py
@@ -1,3 +1,4 @@
+import math
 keylength = [40.0,56,64,112,128]
 time = []
 average = 0
@@ -5,13 +6,15 @@ units = 1000000/(10*10)
 print("Units: ", units)
 
 for i in keylength:
+    average += i
     t = 2**i/(5*10**8)/units/60/60/24
     time.append(t)
     print(i," \t: ",t,"days")
 
-for i in time:
-    average += i
 average /= len(time)
 
-print(average)
+t = (2**average)/(5*10**8)/(1000000000/(10*10))/60/60/24
 
+print(average)
+print(t)
+print(math.log(t,2))
